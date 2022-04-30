8eeccd23db336ad6eb6d52a7dadd69790bb80ced
Emma Rünzel <emma@ruenzel.de>
2022-04-30 19:01:41 +0200

    Seeds User

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require "open-uri"
+
+puts "Cleaning Database! 🧹"
+
+User.destroy_all
+
+puts "Database is clean! 🧼"
+
+User.create!(
+  # username: "Emma",
+  # bio: "Hello, I'm Emma and this is my amazing Bio",
+  email: "emma@test.com",
+  password: "123456",
+  # language: 1,
+  # phone_number: "+90123456789",
+)
+
+puts "🤟🏻🤟🏼🤟🏽🤟🏾🤟🏿 Amazing! We are done 🤟🏻🤟🏼🤟🏽🤟🏾🤟🏿"
