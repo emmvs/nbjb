79633d371d28c55f0f0e780247457f1ac8f553c3
Emma Rünzel <emma@ruenzel.de>
2023-02-21 20:46:30 +0100

    Clickable night

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -2,5 +2,8 @@
 
 class DashboardsController < ApplicationController
   def dashboard
+    @nights = Night.where(
+      start_time: Time.now.beginning_of_month.beginning_of_week..Time.now.end_of_month.end_of_week
+    )
   end
 end
