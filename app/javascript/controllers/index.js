64136f642d5e8ccec3c42edcf4507f427b518b36
Emma Rünzel <emma@ruenzel.de>
2023-02-23 17:51:22 +0100

    try and go pack to the start before geocoder in JS folder

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -10,6 +10,19 @@
 
 
 
+// Load all the controllers within this directory and all subdirectories.
+// Controller files must be named *_controller.js.
+
+import { Application } from "stimulus"
+import { definitionsFromContext } from "stimulus/webpack-helpers"
+
+const application = Application.start()
+const context = require.context("controllers", true, /_controller\.js$/)
+application.load(definitionsFromContext(context))
+
+const channels = require.context('.', true, /_channel\.js$/)
+channels.keys().forEach(channels)
+
 
 
 // Old Code
--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -1,15 +1,15 @@
-import { Controller } from "@hotwired/stimulus"
+// import { Controller } from "@hotwired/stimulus"
 
-// Connects to data-controller="map"
-export default class extends Controller {
-  static values = { apiKey: String }
-  connect() {
-    mapboxgl.accessToken = this.apiKeyValue;
-    var map = new mapboxgl.Map({
-    container: 'map',                                // Container ID
-    style: 'mapbox://styles/mapbox/streets-v11',     // URL Style
-    center: [-75.5, 40],                             // Starting position
-    zoom: 9                                          // Starting zoom
-  });
-  }
-}
+// // Connects to data-controller="map"
+// export default class extends Controller {
+//   static values = { apiKey: String }
+//   connect() {
+//     mapboxgl.accessToken = this.apiKeyValue;
+//     var map = new mapboxgl.Map({
+//     container: 'map',                                // Container ID
+//     style: 'mapbox://styles/mapbox/streets-v11',     // URL Style
+//     center: [-75.5, 40],                             // Starting position
+//     zoom: 9                                          // Starting zoom
+//   });
+//   }
+// }
