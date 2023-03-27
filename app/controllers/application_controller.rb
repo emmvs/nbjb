ecdaf834c73e3ba561f832699e2994b9b23a1a92
Emma Rünzel <emma@ruenzel.de>
2023-03-27 21:49:20 +0200

    Will fix the edit/update of the user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -25,10 +25,8 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    # raise
-    devise_parameter_sanitizer.permit(:sign_up, keys: %i[avatar first_name last_name birthday first_game
-                                                         sign favorite_drink lucky_number bio wins losses])
-    devise_parameter_sanitizer.permit(:account_update, keys: %i[avatar first_name last_name birthday first_game
-                                                                sign favorite_drink lucky_number bio wins losses])
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[avatar first_name last_name birthday first_game sign favorite_drink lucky_number bio wins losses])
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[avatar first_name last_name birthday first_game sign favorite_drink lucky_number bio wins losses])
+    # binding.pry
   end
 end
