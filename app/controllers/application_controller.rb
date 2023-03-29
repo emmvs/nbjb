0e2fa379942e77c107f08ebd4907a98f4822e642
Emma Rünzel <emma@ruenzel.de>
2023-03-29 21:39:16 +0200

    Will update the users to profiles to not confuse future emma with devise users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -27,6 +27,5 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: %i[avatar first_name last_name birthday first_game sign favorite_drink lucky_number bio wins losses])
     devise_parameter_sanitizer.permit(:account_update, keys: %i[avatar first_name last_name birthday first_game sign favorite_drink lucky_number bio wins losses])
-    # binding.pry
   end
 end
